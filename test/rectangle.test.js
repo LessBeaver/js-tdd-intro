230df205d2971e1516daa28fb745530433751892
Leslie <lesliec38@hotmail.fr>
2020-06-15 11:37:59 +0200

    final commit

--- test/rectangle.test.js
+++ test/rectangle.test.js
@@ -6,18 +6,18 @@ const rectangle = new Rectangle(10, 5);
 describe('rectangle', () => {
   // give the test a label using it
   it('should return false', () => {
-    assert.strictEqual(rectangle.isSquare(10, 5), false);
+    assert.strictEqual(rectangle.isSquare(), false);
   });
 
   it('should return true', () => {
-    assert.strictEqual(rectangle.isSquare(10, 5), false);
+    assert.strictEqual(rectangle.isSquare(), false);
   });
 
   it('should return the area', () => {
-    assert.strictEqual(rectangle.getArea(10, 5), 50);
+    assert.strictEqual(rectangle.getArea(), 50);
   });
 
   it('should return the perimeter', () => {
-    assert.strictEqual(rectangle.getPerimeter(10, 5), 30);
+    assert.strictEqual(rectangle.getPerimeter(), 30);
   });
 });
